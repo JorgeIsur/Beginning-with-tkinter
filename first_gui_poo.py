7bd55b82a41f01f99bd5dc4dc806b3ed46e76f9b
Isur <ramirezisur@gmail.com>
2021-07-26 16:51:50 -0500

    Orientado a objetos

--- first_gui_poo.py
+++ first_gui_poo.py
@@ -1,34 +1,112 @@
 from tkinter import *
 from tkinter import ttk
 
-# Crea una clase Python para definir el interfaz de usuario de
-# la aplicación. Cuando se cree un objeto del tipo 'Aplicacion'
-# se ejecutará automáticamente el método __init__() qué 
-# construye y muestra la ventana con todos sus widgets: 
+# La clase 'Aplicacion' ha crecido. En el ejemplo se incluyen
+# nuevos widgets en el método constructor __init__(): Uno de
+# ellos es el botón 'Info'  que cuando sea presionado llamará 
+# al método 'verinfo' para mostrar información en el otro 
+# widget, una caja de texto: un evento ejecuta una acción: 
 
 class Aplicacion():
     def __init__(self):
-        raiz = Tk()
-        raiz.geometry('300x200')
-        raiz.configure(bg = 'beige')
-        raiz.title('Aplicación')
-        ttk.Button(raiz, text='Salir', 
-                   command=raiz.destroy).pack(side=BOTTOM)
-        raiz.mainloop()
-
-# Define la función main() que es en realidad la que indica 
-# el comienzo del programa. Dentro de ella se crea el objeto 
-# aplicación 'mi_app' basado en la clase 'Aplicación':
+        
+        # En el ejemplo se utiliza el prefijo 'self' para
+        # declarar algunas variables asociadas al objeto 
+        # ('mi_app')  de la clase 'Aplicacion'. Su uso es 
+        # imprescindible para que se pueda acceder a sus
+        # valores desde otros métodos:
+        
+        self.raiz = Tk()
+        self.raiz.geometry('300x200')
+        
+        # Impide que los bordes puedan desplazarse para
+        # ampliar o reducir el tamaño de la ventana 'self.raiz':
+        
+        self.raiz.resizable(width=False,height=False)
+        self.raiz.title('Ver info')
+        
+        # Define el widget Text 'self.tinfo ' en el que se
+        # pueden introducir varias líneas de texto:
+        
+        self.tinfo = Text(self.raiz, width=40, height=10)
+        
+        # Sitúa la caja de texto 'self.tinfo' en la parte
+        # superior de la ventana 'self.raiz':
+        
+        self.tinfo.pack(side=TOP)
+        
+        # Define el widget Button 'self.binfo' que llamará 
+        # al metodo 'self.verinfo' cuando sea presionado
+        
+        self.binfo = ttk.Button(self.raiz, text='Info', 
+                                command=self.verinfo)
+        
+        # Coloca el botón 'self.binfo' debajo y a la izquierda
+        # del widget anterior
+                                
+        self.binfo.pack(side=LEFT)
+        
+        # Define el botón 'self.bsalir'. En este caso
+        # cuando sea presionado, el método destruirá o
+        # terminará la aplicación-ventana 'self.raíz' con 
+        # 'self.raiz.destroy'
+        
+        self.bsalir = ttk.Button(self.raiz, text='Salir', 
+                                 command=self.raiz.destroy)
+                                 
+        # Coloca el botón 'self.bsalir' a la derecha del 
+        # objeto anterior.
+                                 
+        self.bsalir.pack(side=RIGHT)
+        
+        # El foco de la aplicación se sitúa en el botón
+        # 'self.binfo' resaltando su borde. Si se presiona
+        # la barra espaciadora el botón que tiene el foco
+        # será pulsado. El foco puede cambiar de un widget
+        # a otro con la tecla tabulador [tab]
+        
+        self.binfo.focus_set()
+        self.raiz.mainloop()
+    
+    def verinfo(self):
+        
+        # Borra el contenido que tenga en un momento dado
+        # la caja de texto
+        
+        self.tinfo.delete("1.0", END)
+        
+        # Obtiene información de la ventana 'self.raiz':
+        
+        info1 = self.raiz.winfo_class()
+        info2 = self.raiz.winfo_geometry()
+        info3 = str(self.raiz.winfo_width())
+        info4 = str(self.raiz.winfo_height())
+        info5 = str(self.raiz.winfo_rootx())
+        info6 = str(self.raiz.winfo_rooty())
+        info7 = str(self.raiz.winfo_id())
+        info8 = self.raiz.winfo_name()
+        info9 = self.raiz.winfo_manager()
+        
+        # Construye una cadena de texto con toda la
+        # información obtenida:
+        
+        texto_info = "Clase de 'raiz': " + info1 + "\n"
+        texto_info += "Resolución y posición: " + info2 + "\n"
+        texto_info += "Anchura ventana: " + info3 + "\n"
+        texto_info += "Altura ventana: " + info4 + "\n"
+        texto_info += "Pos. Ventana X: " + info5 + "\n"
+        texto_info += "Pos. Ventana Y: " + info6 + "\n"
+        texto_info += "Id. de 'raiz': " + info7 + "\n"
+        texto_info += "Nombre objeto: " + info8 + "\n" 
+        texto_info += "Gestor ventanas: " + info9 + "\n"
+        
+        # Inserta la información en la caja de texto:
+        
+        self.tinfo.insert("1.0", texto_info)
 
 def main():
     mi_app = Aplicacion()
     return 0
-
-# Mediante el atributo __name__ tenemos acceso al nombre de un
-# un módulo. Python utiliza este atributo cuando se ejecuta
-# un programa para conocer si el módulo es ejecutado de forma
-# independiente (en ese caso __name__ = '__main__') o es 
-# importado:
 
 if __name__ == '__main__':
     main()
